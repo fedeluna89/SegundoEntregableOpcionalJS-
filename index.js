01234ef38b44ddc3ad46d8cb9aaef051a45bd879
Federico Luna <fedelunacjs@gmail.com>
2022-06-27 18:58:00 -0300

    entregable opcional 2

---
+++ index.js
@@ -0,0 +1,14 @@
+const familia1 = ["claudio", "bettina", "agustin", "matias", "constanza", "federico"];
+console.log(familia1);
+
+familia1.push("sofia", "emma");
+console.log(familia1);
+
+familia1.pop();
+console.log(familia1);
+
+const familia2 = ["cecilia", "agostina", "lucia", "josefina"];
+
+console.log(familia2.join("-"));
+
+console.log(familia1.concat(familia2));
